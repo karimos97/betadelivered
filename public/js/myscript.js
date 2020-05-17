068730ca55bfa9414452d12eaed3f61ad06f36a8
karimos97 <kerimeh599@gmail.com>
2020-05-17 16:36:52 +0100

    change urls to dynamic 2

--- public/js/myscript.js
+++ public/js/myscript.js
@@ -5,14 +5,14 @@ $(document).ready(function() {
     });
     $(document).on("click", ".targe", function() {
         var clickedBtnID = $(this).attr('id');
-        var jqxhr = $.getJSON("http://delivred.test/api/order/" + clickedBtnID, function() {
+        var jqxhr = $.getJSON("/api/order/" + clickedBtnID, function() {
             $('#Ecity').val(jqxhr.responseJSON.city)
             $('#Ecountry').val(jqxhr.responseJSON.country)
             $('#Efull_name').val(jqxhr.responseJSON.full_name)
             $('#Ephone').val(jqxhr.responseJSON.phone)
             $('#Eadress').val(jqxhr.responseJSON.adress)
         })
-        var respo = $.getJSON("http://delivred.test/api/order/details/" + clickedBtnID, function() {
+        var respo = $.getJSON("/api/order/details/" + clickedBtnID, function() {
             $("li").remove();
             dat = respo.responseJSON;
             for (i = 0; i < dat.length; i++) {
