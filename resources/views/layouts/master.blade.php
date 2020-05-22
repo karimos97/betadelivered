96d23a840b75d6fbc963c38aa21eb65814367ab3
Anass <el_moussaoui_anass@hotmail.com>
2020-05-22 17:24:50 +0000

    add bootstrap js libraries

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -43,19 +43,35 @@
 
 
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+
+<!-- bootstrap librabires --> 
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" 
+        crossorigin="anonymous"></script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" i
+        ntegrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" 
+        crossorigin="anonymous"></script>
+
+
+<!-- Datatable librabires --> 
+<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>        
 <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
+
+
 <script src="{{ secure_asset('js/handlebars.js') }}"></script>
+
+
 <script src="https://cdnjs.cloudflare.com/ajax/libs/mark.js/8.0.0/jquery.mark.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/js/select2.min.js"></script>
+
+
 
 @yield('script')
 
 
 <script>
-
-
-
 
 </script>
 </html>
